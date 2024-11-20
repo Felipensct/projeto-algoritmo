f0605b38e5793334cd2a0cf80a5270a59e8f603a
Felipe <felipensct.silva@gmail.com>
2024-11-20 09:40:03 -0300

    Primeiro commit

---
+++ pilhacomlista2/Lista.java
@@ -0,0 +1,36 @@
+package pilhacomlista2;
+
+public class Lista {
+    private Object[] elementos = new Object[100];
+    private int total = 0;
+
+    public void add(Object dados) {
+        this.elementos[this.total] = dados;
+        this.total++;
+    }
+
+    public boolean vazia() {
+        return total == 0;
+    }
+
+    public Object pega(int posicao) {
+        if (posicao < 0 || posicao >= total) {
+            throw new IllegalArgumentException("**POSICAO INVALIDA**");
+        }
+        return elementos[posicao];
+    }
+
+    public int tamanho() {
+        return total;
+    }
+
+    public void remove(int posicao) {
+        if (posicao < 0 || posicao >= total) {
+            throw new IllegalArgumentException("**POSICAO INVALIDA**");
+        }
+        for (int i = posicao; i < total - 1; i++) {
+            elementos[i] = elementos[i + 1];
+        }
+        total--;
+    }
+}
